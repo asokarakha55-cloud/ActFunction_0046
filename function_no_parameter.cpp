227fe76945757dc0322d4dccb6f28846dc89b28f
asokarakha55-cloud <asokarakha55@gmail.com>
2026-03-02 12:14:22 +0700

    first commit

---
+++ function_no_parameter.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+    cout << "Masukkan lebar : ";
+    cin >> lebar;
+}
+
+int luaspersegi()
+{
+    return panjang * lebar;
+}
+
+int kelilingpersegi()
+{
+    return 2 * (panjang + lebar);
+}
+
+void output()
+{
+    cout << "Luas persegi : " << luaspersegi() << endl;
+    cout << "Keliling persegi : " << kelilingpersegi() << endl;
+}
+
+int main()
+{
+    input();
+    output();
+}
